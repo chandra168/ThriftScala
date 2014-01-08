614622c694d0a817557cdd94545d6ee11e275d70
Nimish Gupta <nimish28@gmail.com>
2014-01-07 19:43:40 -0500

    Plumbing done, thrift connected with Javascript client over HTTP

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,32 +1,3 @@
-import org.apache.thrift.transport.TServerSocket;
-import org.apache.thrift.transport.TServerTransport;
-import org.apache.thrift.server.TServer;
-import org.apache.thrift.server.TServer.Args;
-import org.apache.thrift.server.TSimpleServer;
-import org.apache.thrift.protocol.TJSONProtocol;
-
-
-
-/*
-object Server {
-
-  def main (args: Array[String]) {
-
-    // Not sure if val or var should be passed to java 
-    var serverHandler = new ProfileServiceImpl ()
-    var processor = new ProfileService.Processor (serverHandler)
-    var proto     = new TJSONProtocol.Factory ()
-
-    var serverTransport = new TServerSocket (9090);
-    var server = new TSimpleServer (new Args (serverTransport).processor (processor).protocolFactory (proto))
-
-    println ("Starting simple server on port 9090");
-
-    server.serve ()
-  }
-}
- */
-
 import akka.actor.{ActorSystem, Props}
 import akka.io.IO
 import spray.can.Http
--- src/main/scala/ThriftService.scala
+++ src/main/scala/ThriftService.scala
@@ -7,11 +7,39 @@ import spray.util._
 import spray.http._
 import HttpMethods._
 
+import org.apache.thrift.transport.TMemoryBuffer
+import org.apache.thrift.protocol.TJSONProtocol
+
 
 class ThriftService extends Actor {
 
   implicit val timeout: Timeout = 1.second // for the actor 'asks'
   import context.dispatcher // ExecutionContext for the futures and scheduler
+
+  private def thriftRequest (input: Array[Byte]): String = { 
+    try {
+
+      //Input
+      var inbuffer = new TMemoryBuffer (input.length)
+      inbuffer.write (input)
+      var inprotocol = new TJSONProtocol (inbuffer)
+
+      //Output
+      var outbuffer = new TMemoryBuffer (100);
+      var outprotocol = new TJSONProtocol (outbuffer);
+
+      var processor = new ProfileService.Processor(new ProfileServiceImpl ())
+      processor.process (inprotocol, outprotocol)
+
+      val output = new Array[Byte] (outbuffer.length)
+      outbuffer.readAll (output, 0, output.length)
+
+      new String (output, "UTF-8")
+
+    } catch {
+        case ex: Exception  => "Error:"+ ex.getMessage ()
+      }
+  }
 
   def receive = {
 
@@ -19,7 +47,10 @@ class ThriftService extends Actor {
     case _:Http.Connected => sender ! Http.Register (self)
 
     case HttpRequest (POST, Uri.Path ("/"), headers, entity: HttpEntity.NonEmpty, protocol) =>
-    println ("Here")
+      val ip = entity.asString
+      sender ! HttpResponse (entity = thriftRequest (ip.getBytes))
+
+      
   
 
     case _:HttpRequest => sender ! HttpResponse (status = 404, entity = "Unknonw resource!")
