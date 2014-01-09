4c5b0c8a9e5dbcd9673f664faf08d6e11f9d8596
Nimish Gupta <nimish28@gmail.com>
2014-01-08 19:00:49 -0500

    Rough draft of complete server, have not tested it yet

--- src/main/scala/ServerHandler.scala
+++ src/main/scala/ServerHandler.scala
@@ -11,24 +11,59 @@ class ProfileServiceImpl extends ProfileService.Iface {
 
   private val prefix = "ProfileDB/"
 
-  private def userExists (uid: String): Boolean = false
+  private def userExists (uid: String): Boolean = (new File (prefix + uid).exists ())
 
-  def createUser (user: Profile): Boolean = {
-    println ("CreateUser")
+  private def getUser (uid: String) : Option[Profile] = {
+    try {
+       val source = scala.io.Source.fromFile (uid)
+       val bytearray = source.map (_.toByte).toArray
+       source.close ()
 
-    /* Check if file exists, otherwise serialize and create it */
-    if ((new File (prefix + user.uid)).createNewFile ()) {
-      // File creation successfull, user does not exist yet
+       // by default uses a binary protocol
+       var deserializer = new TDeserializer ()
+       var user = new Profile
+       deserializer.deserialize (user, bytearray)
+       Some (user)
+    }
+    catch {
+       case ex: FileNotFoundException => None
+       case ex: IOException => None
+    }
+  }
+
+  private def deleteUser (uid: String) : Boolean = {
+    try {
+      val file = new File (prefix + uid)
+      file.delete ()
+      true
+    }
+    catch {
+       case ex: FileNotFoundException => false
+       case ex: IOException => false
+    }
+  }
+
+  private def putUser (user: Profile) : Boolean = {
 
       var sbarr = (new TSerializer ()).serialize (user)
 
       // Write out to a file
-      val fos = new FileOutputStream (new File (prefix + user.uid))
-      // IOUtils.write (sbarr, fos)
-      fos.write (sbarr)
-      fos.close ()
-
+      val out = new FileOutputStream (new File (prefix + user.uid))
+      out.getChannel ().truncate (0)
+      out.getChannel ().force (true)
+      out.write (sbarr)
+      out.close ()
       true
+  }
+
+
+  def createUser (user: Profile): Boolean = {
+    println ("CreateUser")
+
+    /* Check if file exists, otherwise serialize and create it ATOMICALLY */
+    if ((new File (prefix + user.uid)).createNewFile ()) {
+      // File creation successfull, user does not exist yet
+      this.putUser (user)
     }
     else {
       // File already exists
@@ -40,40 +75,28 @@ class ProfileServiceImpl extends ProfileService.Iface {
   def login (uid: String, password: String): Profile = {
      println ("Login")
 
-     try {
-       val source = scala.io.Source.fromFile (uid)
-       val bytearray = source.map (_.toByte).toArray
-       source.close ()
-
-       // by default uses a binary protocol
-       var deserializer = new TDeserializer ()
-       var user = new Profile
-       deserializer.deserialize (user, bytearray)
-       user
-     } catch {
-       case ex: FileNotFoundException => new Profile
-       case ex: IOException => new Profile
+     getUser (uid) match {
+       case Some (user) => user
+       case None => new Profile
      }
   }
 
 
   def modify (user: Profile): Boolean = {
     println ("Modify")
-    try
-    {
-      scala.io.Source.fromFile (user.uid)
 
-      true
-    }
-    catch
-    {
-      case ex:FileNotFoundException => 
-        false
+    getUser (user.uid) match {
+      case Some (userdb) => putUser (user)
+      case None => false
     }
   }
   
   def remove (user: Profile): Boolean = {
     println ("Remove")
-    true
+
+    getUser (user.uid) match {
+      case Some(userdb) if userdb.password == user.password => deleteUser (user.uid)
+      case None => false
+    }
   }
 }
