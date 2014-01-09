b4ce60a909f2fd11c8a7d7025a4f33caea1b8bf9
Nimish Gupta <nimish28@gmail.com>
2014-01-08 20:16:14 -0500

    client functionality complete

--- client/client_impl.js
+++ client/client_impl.js
@@ -2,6 +2,72 @@ var transport = new Thrift.Transport("/");
 var protocol  = new Thrift.Protocol(transport);
 var client    = new ProfileServiceClient(protocol);
 
+function clear_user () {
+  document.getElementById ("detail_username").value = "";
+  document.getElementById ("detail_name").value     = "";
+  document.getElementById ("detail_password").value = "";
+  document.getElementById ("detail_email").value    = "";
+
+}
+
+
+function get_user () {
+
+  var user = new Profile ();
+  user.uid      = document.getElementById ("detail_username").value;
+  user.name     = document.getElementById ("detail_name").value;
+  user.password = document.getElementById ("detail_password").value;
+  user.email    = document.getElementById ("detail_email").value;
+
+  return user;
+}
+
+
+function put_user (user) {
+  document.getElementById ("detail_username").value = user.uid;
+  document.getElementById ("detail_name").value     = user.name;
+  document.getElementById ("detail_password").value = user.password;
+  document.getElementById ("detail_email").value    = user.email;
+}
+
+
+function logout_view () {
+  clear_user ();
+
+  document.getElementById ("login_prompt_radio").disabled = false;
+  document.getElementById ("new_user_radio").disabled = false;
+
+  document.getElementById ("create_button").disabled = false;
+  document.getElementById ("modify_button").disabled = true;
+  document.getElementById ("delete_button").disabled = true;
+  document.getElementById ("logout_button").disabled = true;
+}
+
+
+function login_view () {
+
+  document.getElementById ("login_prompt_radio").disabled = true;
+  document.getElementById ("new_user_radio").disabled = true;
+
+  document.getElementById ("create_button").disabled = true;
+  document.getElementById ("modify_button").disabled = false;
+  document.getElementById ("delete_button").disabled = false;
+  document.getElementById ("logout_button").disabled = false;
+}
+
+
+function login_prompt () {
+  document.getElementById ("user_div").style.display  = 'none';
+  document.getElementById ("login_div").style.display = 'block';
+}
+
+
+function user_prompt () {
+  document.getElementById ("login_div").style.display = 'none';
+  document.getElementById ("user_div").style.display  = 'block';
+}
+
+
 
 function login_user () {
 
@@ -10,28 +76,24 @@ function login_user () {
 
   try {
     var result = client.login (username.trim (), password.trim());
-    alert ("Login call completed successfully");
+    put_user (result);
+    login_view ();
+    user_prompt ();
   } catch (e) {
-    alert ("Problem completing login call");
+    alert (e.why);
   }
 
   return false;
 }
 
-    
+
 function create_user () {
-
-  var user = new Profile ();
-  user.uid      = "nimishgupta";
-  user.name     = "Nimish";
-  user.password = "Nimish";
-  user.email    = "nimish@cs.umass.edu";
-
   try {
+    var user = get_user ();
     var result = client.createUser (user);
 
     if (result) {
-      alert ("User Created");
+      login_view ();
     }
     else {
       alert ("problem creating user");
@@ -42,3 +104,45 @@ function create_user () {
 
   return false;
 }
+
+
+function modify_user () {
+  try {
+    var user = get_user ();
+    var result = client.modify (user);
+    if (result) {
+      alert ("Modify succeeded");
+    }
+    else {
+      alert ("Modify failed");
+    }
+  }
+  catch (e) {
+    alert (e.why);
+  }
+}
+
+
+function delete_user () {
+  try {
+    var user = get_user ();
+    var result = client.remove (user);
+    if (result) {
+      logout_view ();
+    }
+    else {
+      alert ("delete failed");
+    }
+  }
+  catch (e) {
+    alert (e.why);
+  }
+}
+
+function logout_user () {
+  logout_view ();
+  login_prompt ();
+}
+
+
+
